af2a1f20a50906e7b959426cc31ea1907d7ba47e
Tatin <rtwam@hotmail.fr>
2020-01-15 17:04:42 +0100

    Premier layout fini

--- WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
+++ WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
@@ -13,15 +13,15 @@ public class WLTwitterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout1);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    //    getMenuInflater().inflate(R.menu.menu_main, menu);
+    //    return true;
+    //}
 
 }
