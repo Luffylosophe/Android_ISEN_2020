b2c62752947d73c165de2f3a8ca7abeb69a0b5e9
theophile <theophilevonck@gmail.com>
2020-01-22 10:51:34 +0100

    logout done

--- WLTwitter/WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
+++ WLTwitter/WLTwitter/app/src/main/java/worldline/ssm/rd/ux/wltwitter/WLTwitterActivity.java
@@ -1,21 +1,28 @@
 package worldline.ssm.rd.ux.wltwitter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import worldline.ssm.rd.ux.wltwitter.utils.Constants;
 import worldline.ssm.rd.ux.wltwitter.utils.PreferenceUtils;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class WLTwitterActivity extends AppCompatActivity {
+    private SharedPreferences myPreferences ;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myPreferences = getPreferences(MODE_PRIVATE);
+        editor=myPreferences.edit();
 
         Intent fromIntent = getIntent();
         String Username="";
@@ -34,6 +41,35 @@ public class WLTwitterActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                logout();
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 
+    private void logout(){
+        String username=myPreferences.getString(getString(R.string.username),"");
+        String password=myPreferences.getString(getString(R.string.password),"");
+        editor.putString((getString(R.string.username)),"");
+        editor.commit();
+        editor.putString((getString(R.string.password)),"");
+        editor.commit();
+        finish();
+        setContentView(R.layout.activity_login);
+        return;
+    }
+
+    private Intent getHoIntent(String userName){
+        final Intent homeIntent = new Intent(this, WLTwitterActivity.class);
+        final Bundle extras = new Bundle();
+
+        extras.putString(Constants.Login.EXTRA_LOGIN,userName);
+        homeIntent.putExtras(extras);
+        return homeIntent;
+    }
 }
